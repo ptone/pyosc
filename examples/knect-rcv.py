81d9744bef18114f6e774a9983d4f04563a13bca
Artem Baguinski <artm@v2.nl>
2011-06-26 03:36:02 +0200

    handle all pending requests in a "frame"
    this time we don't return from the frame callback immediatelly, but
    first handle all pending requests (OSC messages). To do that we
    implement a handle_timeout() method in a server instance.

--- examples/knect-rcv.py
+++ examples/knect-rcv.py
@@ -7,6 +7,16 @@ server = OSCServer( ("localhost", 7110) )
 server.timeout = 0
 run = True
 
+# this method of reporting timeouts only works by convention
+# that before calling handle_request() field .timed_out is 
+# set to False
+def handle_timeout(self):
+    self.timed_out = True
+
+# funny python's way to add a method to an instance of a class
+import types
+server.handle_timeout = types.MethodType(handle_timeout, server)
+
 def user_callback(path, tags, args, source):
     # which user will be determined by path:
     # we just throw away all slashes and join together what's left
@@ -27,13 +37,19 @@ server.addMsgHandler( "/user/3", user_callback )
 server.addMsgHandler( "/user/4", user_callback )
 server.addMsgHandler( "/quit", quit_callback )
 
-
+# user script that's called by the game engine every frame
 def each_frame():
-    server.handle_request()
+    # clear timed_out flag
+    server.timed_out = False
+    # handle all pending requests then return
+    while not server.timed_out:
+        server.handle_request()
 
 # simulate a "game engine"
 while run:
+    # do the game stuff:
     sleep(1)
+    # call user script
     each_frame()
 
 server.close()
